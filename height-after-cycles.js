177a83aed6999d2a366c43c5c23215b18b004634
Jason Benn <jasoncbenn@gmail.com>
2015-07-09 17:56:19 -0700

    Convert Height After Cycles to ES6/Immutable.

--- height-after-cycles.js
+++ height-after-cycles.js
@@ -1,30 +1,8 @@
-// 1m sapling, planted at end of winter
-// every spring it doubles
-// every summer it grows a meter
-// people want Xm of a tree
-// given X cycles, how tall is the tree
-
-// num cycles - height
-// 0 1 winter
-// 1 2 spring
-// 2 3 summer
-// 3 6 spring
-// 4 7 summer
-// 5 14 spring
-// 6 15 summer
-// 7 30 spring
-// 8 31 summer
-
-// odd = spring
-// even = summer
-
-
-
-var __ = require('lodash');
+var { Range } = require('immutable');
 
 function assert(actual, expected) {
   if (expected !== actual) {
-    console.log('✘ expected: ' + expected.toString() + ' !== actual: ' + actual.toString())
+    console.log(`✘ expected: #{expected} !== actual: #{actual}`)
   } else {
     console.log('✓')
   }
@@ -43,10 +21,10 @@ function heightAfterCycle(height, cycle) {
 }
 
 function heightAfterCycles(cycles) {
-  return __.reduce(__.range(1, cycles + 1), heightAfterCycle, 1)
+  return Range(1, cycles + 1).reduce(heightAfterCycle, 1)
 }
 
-;[
+[
   [0, 1],
   [1, 2],
   [2, 3],
