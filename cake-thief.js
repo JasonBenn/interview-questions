369357aaf23ca201ffee0bb7f4d36512e1db3d0d
Jason Benn <jasoncbenn@gmail.com>
2015-07-12 13:15:02 -0700

    Refactor Cake Thief.

--- cake-thief.js
+++ cake-thief.js
@@ -2,28 +2,23 @@
 // Strategy that could have worked: start with a tiny piece, build up.
 // Other room for improvement: discuss tradeoffs for an efficient but non-optimal solution.
 // Maybe I could have just filled the knapsack with the most expensive cakes, then the remainder with less efficient cakes - my so called "naive solution" from last time.
-// Where this had a cost of O(n * k) (where n is number of cakes, k is size of knapsack) and a space cost of O(k) (for maxValuesAtCapacities)...
+// Where this had a cost of O(n * k) (where n is number of cakes, k is size of knapsack) and a space cost of O(k) (for allCapacities)...
 // The non-optimal solution had a sort cost of O(nlog(n)) and a space cost of O(n) (for the sorted cakes)
 
-var __ = require('lodash');
+import { Range } from 'immutable'
 
 // Test framework
-function assert(expected, actual) {
-  if (expected !== actual) {
-    console.log('✘ ' + expected.toString() + ' !== ' + actual.toString())
-  } else {
-    console.log('✓')
-  }
+function assert(actual, expected) {
+  console.log(expected === actual ? '✓' : `✘ expected: ${expected} !== actual: ${actual}`)
 }
 
-function selectCake(cakes, duffelCapacity) {
-  var allCapacities = __.range(1, duffelCapacity + 1);
-  var maxValuesAtCapacities = __.reduce(allCapacities, maxValueForCapacity(cakes), {0: 0})
-  return maxValuesAtCapacities[duffelCapacity];
+function selectCake(cakes, capacity) {
+  var allCapacities = Range(1, capacity + 1).reduce(maxValueForCapacity(cakes), {0: 0})
+  return allCapacities[capacity];
 }
 
 function maxValueForCapacity(cakes) {
-  return function(maxValuesAtCapacities, capacity) {
+  return function(allCapacities, capacity) {
     var maxValueForCapacity = 0;
 
     cakes.forEach(function(cake) {
@@ -33,32 +28,35 @@ function maxValueForCapacity(cakes) {
       if (weight > capacity) return;
 
       var remainder = capacity - weight
-      var possibleMaxValue = maxValuesAtCapacities[remainder] + value;
+      var possibleMaxValue = allCapacities[remainder] + value;
       if (possibleMaxValue > maxValueForCapacity) {
         maxValueForCapacity = possibleMaxValue;
       }
     })
 
-    maxValuesAtCapacities[capacity] = maxValueForCapacity;
-    return maxValuesAtCapacities;
+    allCapacities[capacity] = maxValueForCapacity;
+    return allCapacities;
   }
 }
 
 
 var cakes = [[7, 160], [3, 90], [2, 15]]
 
-assert(selectCake(cakes, 0), 0)
-assert(selectCake(cakes, 1), 0)
-assert(selectCake(cakes, 2), 15)
-assert(selectCake(cakes, 3), 90)
-assert(selectCake(cakes, 3), 90)
-assert(selectCake(cakes, 5), 105)
-assert(selectCake(cakes, 7), 180)
-assert(selectCake(cakes, 9), 270)
-assert(selectCake(cakes, 10), 270)
-assert(selectCake(cakes, 11), 285)
-assert(selectCake(cakes, 12), 360)
-assert(selectCake(cakes, 20), 555)
+var tests = [
+  [0, 0],
+  [1, 0],
+  [2, 15],
+  [3, 90],
+  [3, 90],
+  [5, 105],
+  [7, 180],
+  [9, 270],
+  [10, 270],
+  [11, 285],
+  [12, 360], 
+  [20, 555]
+]
+tests.forEach(([capacity, prize]) => assert(selectCake(cakes, capacity), prize))
 
 // goal: two medium value cakes > 1 large value cake, given capacity.
 var closeInValueCakes = [[10, 20], [15, 35]]
