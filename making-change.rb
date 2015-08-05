ad7a0801194f3dd58fb65f57ca374641b9c1eadd
Jason Benn <jasoncbenn@gmail.com>
2015-08-04 18:04:28 -0700

    Giving up on alternative solution to Making Change.

--- making-change.rb
+++ making-change.rb
@@ -2,10 +2,13 @@
 # Did it! Decently elegant recursive solution.
 # Never got memoizing working or the DP solution.
 
+require 'pry'
+
 def initial(arr)
   arr[0..-2]
 end
 
+# Recursive. Memoizable.
 def num_combos(amount, denoms)
   smaller_coins, largest_coin = initial(denoms), denoms.last
 
@@ -23,6 +26,50 @@ def num_combos(amount, denoms)
     end
   end
 end
+
+
+# DP attempt. Demoralizing, unsuccessful.
+# def num_combos(amount, denoms)
+#   # The first group is the smallest denomination, other groups include progressively more coins.
+#   denom_groups = denoms.length.times.map { |n| denoms.slice(0..n) }
+
+#   all_combos = denom_groups.reduce({}) do |args_to_combos, denom_group|
+#     smaller_coins, largest_coin = initial(denom_group), denom_group.last
+
+#     (1..amount).each do |subamount|
+#       args = [subamount, denom_group]
+
+#       if smaller_coins.none?
+#         puts "Computing #{args}."
+#         args_to_combos[args] = subamount % largest_coin == 0 ? 1 : 0
+#         next
+#       end
+
+#       lg_coins_that_fit = subamount / largest_coin
+
+#       args_to_combos[args] = (1..lg_coins_that_fit).reduce(0) do |total, n_large_coins|
+#         remainder = subamount - n_large_coins * largest_coin
+#         puts "Computing #{args}."
+#         # puts "Subamount: #{subamount}. Denom group: #{denom_group}. With #{n_large_coins} of #{largest_coin}c, remainder is #{remainder}."
+#         # binding.pry
+#         case remainder
+#         when 0
+#           # puts "No remainder. Total so far: #{total}"
+#           total + 1
+#         else
+#           # puts "Remainder: #{remainder}. cache[#{remainder}, #{smaller_coins}]: #{args_to_combos[[remainder, smaller_coins]]}. Total is now #{total + args_to_combos[[remainder, smaller_coins]]}"
+#           total + args_to_combos[[remainder, smaller_coins]]
+#         end
+#       end
+#     end
+
+#     args_to_combos
+#   end
+
+#   pp all_combos
+
+#   all_combos[[amount, denoms]]
+# end
 
 require_relative './tiny_test_framework'
 [
