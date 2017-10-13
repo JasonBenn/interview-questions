20ede0c3ee1e94f2cca7fde24079f6499c231130
Jason Benn <jasoncbenn@gmail.com>
2017-10-13 12:36:08 -0700

    Finish restore_ip_addresses.

--- restore_ip_addresses.py
+++ restore_ip_addresses.py
@@ -1,109 +1,92 @@
 from minitest import *
 
 
-def valid_tail(tail, ip_tuples_length):
-    tail_tuple_size = 4 - ip_tuples_length
-    tail_tuple_min_length = 1 * tail_tuple_size
-    tail_tuple_max_length = 4 * tail_tuple_size
-    return tail_tuple_min_length <= len(tail) <= tail_tuple_max_length
+def valid_tail(digits, len_head_tuples, len_last_head_tuple):
+    max_digits = (4 - len_head_tuples) * 3 + 3 - len_last_head_tuple
+    min_digits = (4 - len_head_tuples)
+    return min_digits <= len(digits) <= max_digits
+
+def valid_head(tuples):
+    for tup in tuples:
+        valid_num = int(tup) <= 255 and int(tup) >= 0
+        valid_len = len(str(int(tup))) == len(tup)
+        if not valid_num or not valid_len:
+            return False
+    return True
+
+def valid(tail_digits, head_tuples):
+    len_last_head_tuple = len((head_tuples or [""])[-1])
+    return valid_tail(tail_digits, len(head_tuples), len_last_head_tuple) and valid_head(head_tuples)
+
+def extend_last_head_tuple(tail_digits, head_tuples):
+    return tail_digits[1:], head_tuples[:-1] + [head_tuples[-1] + tail_digits[0]]
 
 
-def could_add_tuple_with_tail(tail):
-    return tail.startswith('0') or tail.startswith('1') or tail.startswith('2')
+def create_new_tuple(tail_digits, head_tuples):
+    return tail_digits[1:], head_tuples + [tail_digits[0]]
+
+def children(tail_digits, head_tuples):
+    if len(head_tuples) is 0:
+        extension_techniques = [create_new_tuple]
+    elif len(tail_digits) is 0:
+        extension_techniques = []
+    else:
+        extension_techniques = [extend_last_head_tuple, create_new_tuple]
+
+    for extension_technique in extension_techniques:
+        candidate_tail, candidate_head = extension_technique(tail_digits, head_tuples)
+
+        if valid(candidate_tail, candidate_head):
+            yield candidate_tail, candidate_head
+
+def ip_address_combinations(tail_digits, head_tuples=[], ip_addrs=None):
+    if ip_addrs is None:
+        # DANGER: lists as default arguments are risky - the default arg will have been mutated for the next test!
+        ip_addrs = []
+
+    if len(tail_digits) is 0:
+        ip_addrs += [".".join(head_tuples)]
+
+    for tail_digits, head_tuples in children(tail_digits, head_tuples):
+        ip_address_combinations(tail_digits, head_tuples, ip_addrs)
+
+    return ip_addrs
 
 
-def head_tail(seq):
-    # print('seq', seq)
-    try:
-        return seq[0], seq[1:]
-    except IndexError:
-        return "", []
+test_equals(extend_last_head_tuple("000", ["0"]), ("00", ["00"]))
 
+test_equals(create_new_tuple("00", []), ("0", ["0"]))
+test_equals(create_new_tuple("000", ["0"]), ("00", ["0", "0"]))
 
-def first_seq_last(seq):
-    try:
-        return seq[:-1], seq[-1]
-    except IndexError:
-        return [], ""
+test_equals(valid("", ["0", "0", "0", "0"]), True)  # THIS GUY
+test_equals(valid("00", ["0", "0"]), True)
+test_equals(valid("0000", []), True)
+test_equals(valid("", ["0", "0", "0", "0", "0"]), False)
+test_equals(valid("0000", []), True)
+test_equals(valid("00", ["0"]), False)
+test_equals(valid("", [""]), False)
 
-# Rewrite as tree with three leaves per node. Cool.
+test_equals(valid_head(["000"]), False)
+test_equals(valid_head(["000"]), False)
+test_equals(valid_head(["0"]), True)
+test_equals(valid_head(["300"]), False)
+test_equals(valid_head(["130"]), True)
+test_equals(valid_head(["255"]), True)
+test_equals(valid_head(["256"]), False)
+test_equals(valid_head(["0256"]), False)
+test_equals(valid_head(["1"]), True)
 
+test_equals(list(children("000", ["0"])), [("00", ["0", "0"])])
+test_equals(
+    list(
+        children("0000", ["1"])
+    ),
+    [
+        ("000", ["10"]),
+        ("000", ["1", "0"]),
+    ]
+)
 
-def ip_address_combinations(digits, ip_tuples=[], solutions=[]):
-    if not len(digits):
-        new_solution = ".".join(ip_tuples)
-        print("if no tail, return solutions + ip_tuples", solutions + [new_solution])
-        return solutions + [new_solution]
-
-    if not valid_tail(digits, len(ip_tuples)):
-        print("RETURN: digits not valid for %s\n" % len(ip_tuples))
-        return None
-
-    head, tail = head_tail(digits)
-    first_seq, last = first_seq_last(ip_tuples)
-
-    ip_tuples_new_tuple = ip_tuples + [digits[0]]
-    ip_tuples_expanded_tuple = first_seq + [last + digits[0]]
-
-    print("head", head, "tail", tail)
-
-    if could_add_tuple_with_tail(tail):
-        print("tail", tail, "ip_tuples_new_tuple", ip_tuples_new_tuple, "\n")
-        return ip_address_combinations(tail, ip_tuples_new_tuple, solutions)
-
-    print("tail", tail, "ip_tuples_expanded_tuple", ip_tuples_expanded_tuple, "\n")
-    return ip_address_combinations(tail, ip_tuples_expanded_tuple, solutions)
-
-
-test_equals(valid_tail("0000", 3), True)
-test_equals(valid_tail("0000", 1), True)
-test_equals(valid_tail("00000", 4), False)
-test_equals(valid_tail("00", 1), False)
-test_equals(valid_tail("000", 1), True)
-test_equals(valid_tail("500", 1), False)
-test_equals(valid_tail("300", 1), False)
-
+test_equals(ip_address_combinations("0", ["0", "0", "0"]), ["0.0.0.0"])
 test_equals(ip_address_combinations("0000"), ["0.0.0.0"])
-test_equals(ip_address_combinations("25525511135"), ["255.255.11.135", "255.255.111.35"])
-
-
-
-# IF NO TAIL
-    # RETURN solutions + ip_tuples
-
-# IF TAIL IS INVALID
-    # RETURN None
-
-# RETURN new head tuple
-# RETURN expanded head tuple
-
-
-
-
-
-
-
-    # Is this our head pass?
-    # if not len(ip_tuples):
-    #     ip_tuples.append(tail_head)
-    #     print("this is our head pass. ip_tuples", ip_tuples, "tail_tail", tail_tail)
-
-    # # return IF no digits remain in tail
-    # if not len(tail_tail):
-    #     final = ip_tuples + [tail_head]
-    #     print("joining and returning:", final)
-    #     solutions = solutions + [".".join(final)]
-    #     return solutions
-
-    # print("ip_tuples len: %s. tail_tail %s is valid: %s" % (len(ip_tuples), tail_tail, valid_tail(tail_tail, len(ip_tuples))))
-    # if valid_tail(tail_tail, len(ip_tuples)):
-    #     ip_tuples = ip_tuples[0:-1] + [ip_tuples[-1] + tail_head]
-    #     print("RECURSE with expanded ip_tuples:", ip_tuples, "tail_tail", tail_tail, "\n")
-    #     return ip_address_combinations(tail_tail, ip_tuples, solutions)
-    # else:
-    #     new_ip_tuples = ip_tuples + [tail_head]
-    #     print("RECURSE with new ip_tuples", new_ip_tuples, tail_tail, "\n")
-    #     return ip_address_combinations(tail_tail, new_ip_tuples, solutions)
-
-
-# wach --only restore_ip_addresses.py "python restore_ip_addresses.py"
+test_equals(ip_address_combinations("23423412135"), ["234.234.121.35", "234.234.12.135"])
